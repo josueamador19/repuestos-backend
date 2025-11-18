6504bcd1534e3288dee58069261c93ab59afa4cb
josueamador19 <amadoroscar789@gmail.com>
2025-11-18 13:58:32 -0600

    archivos para el deploy

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles  
+
 from routers import productos, blog, comentarios, contacto, faq, politicas
-from routers import productos
 from routers import login
 from routers.pedido import router_pedidos 
-
 
 from db import get_connection
 
 app = FastAPI(title="Repuestos API", version="1.0")
 
-# Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=[
+        "*"
+        # "http://localhost:5173",
+        # "https://tu-frontend.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Archivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Routers
 app.include_router(productos.router)
 app.include_router(blog.router)
 app.include_router(comentarios.router)
@@ -29,7 +34,6 @@ app.include_router(router_pedidos)
 app.include_router(login.router)
 app.include_router(politicas.router)
 app.include_router(faq.router)
-
 
 @app.get("/")
 def root():
