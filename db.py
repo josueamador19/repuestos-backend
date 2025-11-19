aa059524979913617d037a1c55cf40696b44439f
wilmoralez <129712492+wilmoralez@users.noreply.github.com>
2025-11-18 18:59:43 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,30 +1,24 @@
 import os
-import pyodbc
+import pymssql
 from dotenv import load_dotenv
 
-load_dotenv()  # solo necesario para pruebas locales
+load_dotenv()
 
 server = os.getenv("DB_SERVER")
-port = os.getenv("DB_PORT", "1433")
+port = int(os.getenv("DB_PORT", "1433"))
 database = os.getenv("DB_NAME")
-driver = os.getenv("DB_DRIVER", "FreeTDS")
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 
-extra_params = "Encrypt=no;TrustServerCertificate=yes;"
-
 def get_connection():
     try:
-        # Conexión solo con usuario/clave
-        connection_string = (
-            f"DRIVER={{{driver}}};"
-            f"SERVER={server};PORT={port};"
-            f"DATABASE={database};"
-            f"UID={user};"
-            f"PWD={password};"
-            f"{extra_params}"
+        conn = pymssql.connect(
+            server=server,
+            port=port,
+            user=user,
+            password=password,
+            database=database
         )
-        conn = pyodbc.connect(connection_string)
         return conn
     except Exception as e:
         print("Error al conectar:", e)
