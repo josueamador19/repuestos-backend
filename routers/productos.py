0fa7da355ed38c09a75f063bd387eb94d2df0a1d
josueamador19 <amadoroscar789@gmail.com>
2025-11-19 15:01:24 -0600

    test de imagenes

--- routers/productos.py
+++ routers/productos.py
@@ -22,7 +22,7 @@ def listar_productos():
                 "Precio": r[3],
                 "Stock": r[4],
                 "Categoria": r[5],
-                "ImagenURL": f"http://127.0.0.1:8000/static/images/{r[6]}" if r[6] else None
+                "ImagenURL": {r[6]} if r[6] else None
             })
         return {"productos": productos}
     except Exception as e:
